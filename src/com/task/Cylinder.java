37a0e26342d9e8ed6d46cbeff302692782d6bdf7
saravanaguhan19 <saravanaguhan@gmail.com>
2023-06-23 18:17:36 +0530

    data hiding task done

---
+++ src/com/task/Cylinder.java
@@ -0,0 +1,53 @@
+package com.task;
+
+public class Cylinder {
+	private double radius;
+	private double height;
+	
+	public double getRadius() {
+		return radius;
+	}
+	public double getHeight() {
+		return height;
+	}
+	public void setRadius(double r) {
+		if(r >= 0) {
+		radius = r;
+		}else {
+			radius = 0;
+		}
+	}
+	public void setHeight(double h) {
+		if(h >= 0) {
+		height = h;
+		}else {
+			height = 0;
+		}
+	}
+
+	public double volume() {
+		return (22/7)*radius*radius*height;
+	}
+	public double curvedSurfaceArea() {
+		return 2*(22/7)*radius*height;
+	}
+	
+	public double totalSurfaceArea() {
+		return 2*(22/7)*radius*(radius+height);
+	}
+	public double perimeter() {
+		return (4*radius)+(4*height);
+	}
+	
+	public static void main(String[] args) {
+		Cylinder obj = new Cylinder();
+		
+		obj.setHeight(2.0);
+		obj.setRadius(2.0);
+		
+		System.out.println("volume:-" +  obj.volume());
+		System.out.println("perimeter:-" + obj.perimeter());
+		System.out.println("curved surface area:-" + obj.curvedSurfaceArea());
+		System.out.println("total surface area:- " + obj.totalSurfaceArea());
+	}
+}
